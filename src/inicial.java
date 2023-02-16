34fe1c931f2768db1a1dd196c65e93e22779edf2
Jordi Ribellas <ribellas2002@gmail.com>
2023-02-16 16:55:02 +0100

    Pantalla Principal, nomes falta el boto de baix i tota la lògica

--- src/inicial.java
+++ src/inicial.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -26,93 +27,79 @@ public class inicial extends JFrame {
 		
 		JLabel textBenvinguda = new JLabel("Benvingut al Trivial");
 		JLabel subTextBenvinguda = new JLabel("Introduïu els noms dels Jugadors per a poder jugar");
+		
+		textBenvinguda.setFont(new Font("Tahoma",Font.BOLD, 32));
+		subTextBenvinguda.setFont(new Font("Tahoma",Font.BOLD, 32));
+		
+		
 		JLabel j1 = new JLabel("Jugador 1");
 		JLabel j2 = new JLabel("Jugador 2");
 		
-		JTextField j1JTF = new JTextField();
-		JTextField j2JTF = new JTextField();
+		j1.setFont(new Font("Tahoma",Font.BOLD, 24));
+		j2.setFont(new Font("Tahoma",Font.BOLD, 24));
+		
+		JTextField j1JTF = new JTextField(10);
+		JTextField j2JTF = new JTextField(10);	
 		
 		Container cp = getContentPane();
 		
 		//Benvinguda
-		benvinguda.setLayout(new GridBagLayout());		
-		GridBagConstraints gridTextBenvinguda = new GridBagConstraints();
-		gridTextBenvinguda.gridx = 0;
-		gridTextBenvinguda.gridy = 0;
-		gridTextBenvinguda.insets = new Insets(0, 0, 0, 0);
-		benvinguda.add(textBenvinguda, gridTextBenvinguda);
+		benvinguda.add(textBenvinguda);
+		benvinguda.add(subTextBenvinguda);
 		
-		GridBagConstraints gridSubTextBenvinguda = new GridBagConstraints();
-		gridSubTextBenvinguda.gridx = 0;
-		gridSubTextBenvinguda.gridy = 1;
-		gridSubTextBenvinguda.insets = new Insets(0, 0, 0, 0);
-		benvinguda.add(subTextBenvinguda, gridSubTextBenvinguda);
+		j1panel.add(j1);
+		j2panel.add(j2);
+
+		j1panelResposta.add(j1JTF);
+		j2panelResposta.add(j2JTF);
 		
-		//Espais de text que posa jugador
-		j1panel.setLayout(new GridBagLayout());
-		GridBagConstraints gridJugador1 = new GridBagConstraints();
-		gridJugador1.gridx = 0;
-		gridJugador1.gridy = 0;
-		gridJugador1.insets = new Insets(0, 0, 0, 0);
-		j1panel.add(j1, gridJugador1);
-		
-		j2panel.setLayout(new GridBagLayout());
-		GridBagConstraints gridJugador2 = new GridBagConstraints();
-		gridJugador2.gridx = 0;
-		gridJugador2.gridy = 0;
-		gridJugador2.insets = new Insets(0, 0, 0, 0);
-		j2panel.add(j2, gridJugador2);
-		
-		
-		j1JTF.setLayout(new GridBagLayout());
-		GridBagConstraints gridj1JTF = new GridBagConstraints();
-		gridj1JTF.gridx = 0;
-		gridj1JTF.gridy = 0;
-		gridj1JTF.insets = new Insets(0, 0, 0, 0);
-		j1panelResposta.add(j1JTF, gridj1JTF);
-		
-		j2JTF.setLayout(new GridBagLayout());
-		GridBagConstraints gridj2JTF = new GridBagConstraints();
-		gridj2JTF.gridx = 0;
-		gridj2JTF.gridy = 0;
-		gridj2JTF.insets = new Insets(0, 0, 0, 0);
-		j2panelResposta.add(j2JTF, gridj2JTF);
 		
 		cp.setLayout(new GridBagLayout());
 		
-		benvinguda.setPreferredSize(new Dimension(300, 120));
+		benvinguda.setPreferredSize(new Dimension(1000, 120));
 		
-		j1panel.setPreferredSize(new Dimension(100, 50));
-		j2panel.setPreferredSize(new Dimension(100, 50));
+		j1panel.setPreferredSize(new Dimension(150, 50));
+		j2panel.setPreferredSize(new Dimension(150, 50));
 		
 		GridBagConstraints espaiBenvinguda = new GridBagConstraints();
 		espaiBenvinguda.insets = new Insets(-500, 0, 0, 0);
 		
 		GridBagConstraints espaiJ1 = new GridBagConstraints();
 		espaiJ1.gridx = 0;
-		espaiJ1.gridy = 1;
-		espaiJ1.insets = new Insets(-100, -1000, 0, 0);
+		espaiJ1.gridy = 0;
+		espaiJ1.insets = new Insets(0, 0, 200, 800);
 		
 		GridBagConstraints espaiJ2 = new GridBagConstraints();
-		espaiJ2.gridx = 1;
-		espaiJ2.gridy = 1;
-		espaiJ2.insets = new Insets(-100, 0, 0, -700);
+		espaiJ2.gridx = 0;
+		espaiJ2.gridy = 0;
+		espaiJ2.insets = new Insets(0, 800, 200, 0);
 		
+		GridBagConstraints espaiJ1Resposta = new GridBagConstraints();
+		espaiJ1Resposta.gridx = 0;
+		espaiJ1Resposta.gridy = 0;
+		espaiJ1Resposta.insets = new Insets(0, 0, 50, 800);
 		
+		GridBagConstraints espaiJ2Resposta = new GridBagConstraints();
+		espaiJ2Resposta.gridx = 0;
+		espaiJ2Resposta.gridy = 0;
+		espaiJ2Resposta.insets = new Insets(0, 800, 50, 0);
 		
-		benvinguda.setBackground(Color.decode("#567189"));
-		
-		j1panel.setBackground(Color.decode("#A7727D"));
-		j1panel.setBorder(BorderFactory.createLineBorder(Color.black));
-		
-		j2panel.setBackground(Color.decode("#698269"));
-		j2panel.setBorder(BorderFactory.createLineBorder(Color.black));
-		
+		benvinguda.setBackground(Color.decode("#F9F5E7"));
+		j1panel.setBackground(Color.decode("#F9F5E7"));
+		j2panel.setBackground(Color.decode("#F9F5E7"));
+		j1panelResposta.setBackground(Color.decode("#F9F5E7"));
+		j2panelResposta.setBackground(Color.decode("#F9F5E7"));
 		cp.setBackground(Color.decode("#F9F5E7"));
 		
+		
 		cp.add(benvinguda, espaiBenvinguda);
+		
 		cp.add(j1panel, espaiJ1);
+		
+		
 		cp.add(j2panel, espaiJ2);
+		cp.add(j1panelResposta, espaiJ1Resposta);
+		cp.add(j2panelResposta, espaiJ2Resposta);
 
 	}
 
